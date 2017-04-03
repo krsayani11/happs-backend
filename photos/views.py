16c424c0b7701f1c335ed75554896cc4f382be87
Karim Sayani <krsayani11@gmail.com>
2017-04-03 03:53:07 -0400

    added queries to photos and users

--- photos/views.py
+++ photos/views.py
@@ -3,7 +3,13 @@ from rest_framework import routers, serializers, viewsets
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Photo
+from django_filters.rest_framework import DjangoFilterBackend
 
 class PhotoViewSet(viewsets.ModelViewSet):
 	queryset = Photo.objects.all()
 	serializer_class = PhotoSerializer
+	filter_backends = (DjangoFilterBackend,)
+    filter_fields = ('username',
+    	'created',
+    	'event_id',
+    	'datafile')
--- users/views.py
+++ users/views.py
@@ -3,10 +3,17 @@ from rest_framework import routers, serializers, viewsets
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import User, Friends
+from django_filters.rest_framework import DjangoFilterBackend
 
 class UserViewSet(viewsets.ModelViewSet):
 	queryset = User.objects.all()
 	serializer_class = UserSerializer
+	filter_backends = (DjangoFilterBackend,)
+    filter_fields = ('name',
+    	'username',
+    	'user_id',
+    	'authentication_token'
+    	'picture')
 	
 class FriendsViewSet(viewsets.ModelViewSet):
 	queryset = Friends.objects.all()
