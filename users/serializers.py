ac96c1a346cc99639a98117a08715eed2c3eb7ff
Karim Sayani <krsayani11@gmail.com>
2017-04-03 04:52:35 -0400

    added all fields for user and friends

--- users/serializers.py
+++ users/serializers.py
@@ -4,12 +4,12 @@ from .models import User, Friends, Friendship
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('name', 'username', 'user_id', 'authentication_token', 'picture')
+        fields = '__all__'
 
 class FriendsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Friends
-        fields = ('members')
+        fields = '__all__'
 
 class FriendshipSerializer(serializers.ModelSerializer):
     class Meta:
