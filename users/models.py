2c518dd6cb97848580245b748e751cd912d1bdb8
Karim Sayani <krsayani11@gmail.com>
2017-04-03 02:30:13 -0400

    second commit

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 from django.db import models
+from django.core.validators import URLValidator
 
 class User(models.Model):
 	name = models.CharField(max_length=255)
