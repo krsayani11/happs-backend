16d204f2dbb64facf22b53faf41528bbc024c32b
Karim Sayani <krsayani11@gmail.com>
2017-04-05 01:27:08 -0400

    trying again

--- users/views.py
+++ users/views.py
@@ -17,7 +17,8 @@ class UserViewSet(viewsets.ModelViewSet):
     filter_fields = ('name',
         'username',
         'user_id',
-        'picture')
+        'picture'
+        'authentication_token')
     
 class UserDetailAPIView(RetrieveAPIView):
     queryset = User.objects.all()
